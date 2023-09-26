a93ae57fe61fb9a7d6afa831980b47f58ef1d871
Brucks-ke <matigrovidsj@gmail.com>
2023-09-26 16:28:37 +0800

    feat: 添加新的ESLint配置

---
+++ .husky/lintstagedrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // "*.{js,jsx,ts,tsx}": [ "prettier --write"],
+  // "{!(package)*.json}": ["prettier --write--parser json"],
+  // "package.json": ["prettier --write"],
+  // "*.vue": ["prettier --write", "stylelint --fix"],
+  // "*.{vue,css,scss,postcss,less}": ["stylelint --fix", "prettier --write"],
+  // "*.md": ["prettier --write"]
+  "**/*": []
+};
